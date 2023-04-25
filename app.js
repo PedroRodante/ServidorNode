5a4db1c3a88df75744e29f03d08a03d90b2ee8bc
PedroRodante <pedrorodantevicente@gmail.com>
2023-04-24 22:40:10 -0300

    Aulinha Victor

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req,res){
-    res.header("Accsess-Control-Allow-Origin","*");
-    res.send("CHEGUEI FILHO DA P***");
+    res.header("Access-Control-Allow-Origin","*");
+    res.send("CHEGUEI!!");
+});
+
+app.post("/dado", function (req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(req.body);
+    console.log("Recebi um dado");
+    console.log(req.dado);
+    res.send("JSON RECEBIDO!");
 });
 
 app.listen(port, () => {
-    console.log('Servidor Rodante com a porta ${port}')
+    console.log(`Servidor Rodante com a porta ${port}`)
 });
+
