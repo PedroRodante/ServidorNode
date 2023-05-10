93a7f9879cd9a7c4b4f40f3cc75a28fd15370fe7
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-09 22:19:18 -0300

    fiz coisas

--- app.js
+++ app.js
@@ -32,13 +32,43 @@ app.post("/cadastro_usuario", function (req, res){
         {
             res.send(err);
         }
-        res.send("Usúario adicionado!")
+        console.log("Usuário adicionado.");
+        res.send("Usuário adicionado!")
+    });
+});
+
+app.get('/login', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(req.query);
+    console.log("Realizando login");
+    
+    let email = req.query.email;
+    let senha = req.query.senha;
+
+    let sql = `SELECT * FROM Usuarios WHERE email = "${email}"`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            res.send(err);
+        }
+        if (rows.length === 0) {
+            console.log("Usuário não encontrado.");
+            res.send("Usuário não encontrado.");
+        } else {
+            console.log("Usuário encontrado.");
+            if (senha === rows[0].senha) {
+                console.log("Senha correta.");
+                res.send("Login realizado com sucesso!");
+            } else {
+                console.log("Senha incorreta.");
+                res.send("Senha incorreta.");
+            }
+        }
     });
 });
 
 app.get("/todos_usuarios", function(req,res){
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("Estou aqui!");
+    console.log("Atualizei os Usuários");
     db.all(`SELECT * FROM Usuarios`, [], (err, rows) =>
     {
         if(err)
